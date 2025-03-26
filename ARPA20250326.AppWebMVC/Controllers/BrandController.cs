3ce9a9cc65153c4a276e30c4b29ffe832cc70ab7
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-03-26 09:12:10 -0600

    Creación de la vista index e inclui la paginación tanto el diseño como la funcionalidad en el controllador

--- ARPA20250326.AppWebMVC/Controllers/BrandController.cs
+++ ARPA20250326.AppWebMVC/Controllers/BrandController.cs
@@ -19,9 +19,16 @@ namespace ARPA20250326.AppWebMVC.Controllers
         }
 
         // GET: Brand
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(Brand brand, int topRegistro = 10)
         {
-            return View(await _context.Brands.ToListAsync());
+            var query = _context.Brands.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(brand.BrandName))
+                query = query.Where(s => s.BrandName.Contains(brand.BrandName));
+            if (topRegistro > 0)
+                query = query.Take(topRegistro);
+
+
+            return View(await query.ToListAsync());
         }
 
         // GET: Brand/Details/5
