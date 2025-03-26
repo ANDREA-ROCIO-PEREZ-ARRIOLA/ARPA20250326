33bc05b674a75c2ffe4987a9cc24f9cd715f1d0c
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-03-26 08:12:05 -0600

    Se colocaron las validaciones a cada uno de los modelos.

--- ARPA20250326.AppWebMVC/Models/Brand.cs
+++ ARPA20250326.AppWebMVC/Models/Brand.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ARPA20250326.AppWebMVC.Models;
 
 public partial class Brand
 {
+    [Display(Name = "Id")]
     public int Id { get; set; }
-
+    [Display(Name = "Nombre")]
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string BrandName { get; set; } = null!;
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
--- ARPA20250326.AppWebMVC/Models/Warehouse.cs
+++ ARPA20250326.AppWebMVC/Models/Warehouse.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ARPA20250326.AppWebMVC.Models;
 
 public partial class Warehouse
 {
+    [Display(Name = "Id")]
     public int Id { get; set; }
-
+    [Display(Name = "Nombre")]
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string WarehouseName { get; set; } = null!;
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
